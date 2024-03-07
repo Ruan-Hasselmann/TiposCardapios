45164be8d8dbb544f68558773e0efe3d662e68ea
Ruan <ruanhasselmanndasilva@gmail.com>
2024-03-07 13:42:46 -0300

    Commit

--- src/test/java/atos/tiposcardapios/TiposCardapiosApplicationTests.java
+++ src/test/java/atos/tiposcardapios/TiposCardapiosApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TiposCardapiosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
